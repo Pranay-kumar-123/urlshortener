015bba7495f1f3b2ef5ada9df4b180e568d3f241
Pranay-kumar-123 <vorugantipranay@gmail.com>
2021-07-21 10:08:04 +0530

    ..

--- server.js
+++ server.js
@@ -1,51 +1,45 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+var mongoose= require('mongoose');
+//const bodyParser = require("body-parser");
+var path = require('path');
+var apis= require('./backend/api/allapiroutes.js');
 
-mongoose.connect('mongodb+srv://HAnWNJS3Mj7U5oem:pedhu@cluster0.flgd8.mongodb.net/authDB?retryWrites=true&w=majority', {});
-mongoose.connection.on('open', function(){
-    console.log("MongoDB Connected ");
-})
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname+'/frontend'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname+'/frontend/index.html');
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
+
+//DataBase Connection
+var Connection_String="mongodb+srv://nikhil_mohan:uDiD2RTJNQMh3ghL@cluster0.ooac4.mongodb.net/Courses?retryWrites=true&w=majority";
+var options={useUnifiedTopology: true, useNewUrlParser: true };
+mongoose.connect(Connection_String,options,function cb(){
+    console.log(Connection_String);
 });
-app.get('/login', function(req, res){
-    res.sendFile(__dirname+'/frontend/login.html'); 
-  });
-  app.get('/register', function(req, res){
-    res.sendFile(__dirname+'/frontend/register.html');
-  });
-  app.get('/succes', function(req, res){
-    res.sendFile(__dirname+'/frontend/succes.html');
-  });
-  app.get('/error', function(req, res){
-    res.sendFile(__dirname+'/frontend/error.html');
-  });
-  var userschema=mongoose.Schema(
-    {
-      username:String,
-      email:String,
-      password:String
-    }
-  );
-  var usermodel=mongoose.model("auth", userschema);
-  app.post("/api/register", (req,res)=>{
-    var user=usermodel(req.body);
-    user.save();
-    res.redirect("/login");
-  })
-  app.post("/api/login", (req,res)=>{
-    usermodel.find(req.body,(err,userdetails)=>{
-      if(userdetails.length>0)
-      res.redirect("/succes");
-      else res.redirect("/error");
-    })
-    //res.redirect("/");
-  })
-var port= process.env.PORT  || 4000;
-app.listen(port, function(){
-    console.log("Site Running on http://localhost:"+port);
-}); 
+mongoose.connection.on('connected', function()
+{console.log("Database Connected");})
+
+app.get('/su/:id', function(req, res){
+    res.sendFile(__dirname+ '/frontend/html/su.html'); 
+ })
+app.get('/', function(req, res){
+   res.sendFile(__dirname+ '/frontend/html/resume.html'); 
+})
+
+app.use(express.static(__dirname+'/frontend'));
+app.use('/api',apis);
+
+app.get('/:page', function(req, res){
+    var ext = path.extname(req.params.page);
+    // console.log(ext);
+    if(ext=="")
+    res.sendFile(__dirname+ '/frontend/html/'+ req.params.page+".html");
+    //else  res.sendFile(__dirname+ '/frontend/'+ req.params.page);
+})
+
+var port= process.env.PORT  || 3000;
+app.listen(port,function cb()
+{console.log("http://localhost:"+port)
+});
